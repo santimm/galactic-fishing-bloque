85c945eb15d0ab2bb50b48286d6d449e38939609
santiago91 <smmdigitaldreams@gmail.com>
2025-05-04 13:40:20 -0500

    ✨ add fetching data to leaderboardList and marketList components

--- src/components/leaderboardList.vue
+++ src/components/leaderboardList.vue
@@ -1,13 +1,32 @@
 <template>
-  <h2>LeaderboardList component</h2>
+  <div>
+    <h2>Leaderboard</h2>
+    <ul>
+      <li v-for="player in players" :key="player.username">
+        <span v-if="player.xp > 0">
+          {{ player.rank }} - {{ player.level }} - {{ player.username }} - {{ player.xp }} -
+          {{ player.gold }}
+        </span>
+      </li>
+    </ul>
+  </div>
 </template>
 
-<script>
-export default {
-  setup() {
-    return {}
+<script setup>
+import { ref, onMounted } from 'vue'
+
+const players = ref([])
+const urlLeaderboard = 'https://api-game.bloque.app/game/leaderboard'
+
+onMounted(async () => {
+  try {
+    const response = await fetch(urlLeaderboard)
+    const data = await response.json()
+    players.value = data.players
+  } catch (error) {
+    console.error('1: Error fetching leaderboard data:', error)
   }
-}
+})
 </script>
 
 <style lang="scss" scoped></style>
--- src/components/marketList.vue
+++ src/components/marketList.vue
@@ -1,7 +1,27 @@
 <template>
-  <h2>MarketList component</h2>
+  <h2>Market</h2>
+  <ul>
+    <li v-for="item in items" :key="item.id">
+      <span>{{ item.name }} - {{ item.type }} - {{ item.description }} </span>
+    </li>
+  </ul>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref, onMounted } from 'vue'
+
+const items = ref([])
+const urlMarket = 'https://api-game.bloque.app/game/market'
+
+onMounted(async () => {
+  try {
+    const response = await fetch(urlMarket)
+    const data = await response.json()
+    items.value = data.items
+  } catch (error) {
+    console.error('1: Error fetching market data:', error)
+  }
+})
+</script>
 
 <style lang="scss" scoped></style>
